755d485a88d9afd7cc39ed1e1bbd217250173d34
JeonMinJae <ssi02014@naver.com>
2021-04-07 15:13:06 +0900

    Add(minjae/UI): Title

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import React from 'react';
-import {Text} from 'react-native';
+import { StatusBar } from 'react-native';
 import styled, { ThemeProvider } from 'styled-components/native';
 import {theme} from './theme';
 
-const Container = styled.View`
+const Container = styled.SafeAreaView`
     flex: 1;
     background-color: ${({ theme }) => theme.background};
     align-items: center;
-    justify-content: center;
+    justify-content: flex-start;
 `;
+
+const Title = styled.Text`
+    font-size: 40px;
+    font-weight: 600;
+    color: ${({ theme }) => theme.main};
+    align-self: flex-start;
+    margin: 0px 20px;
+`;
+
 
 export default function App() {
     return (
         <ThemeProvider theme={theme}>
             <Container>
-                <Text>hi</Text>
+                <StatusBar
+                    barStyle="light-content"
+                    backgroundColor={theme.background}
+                />
+                <Title>TODO List</Title>
             </Container>
         </ThemeProvider>
     )
